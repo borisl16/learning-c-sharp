d53014df789671447efc567638e7efa288931c6b
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-10-18 12:58:03 +0100

    updates

--- Start/Overview/VarsAndData/Program.cs
+++ Start/Overview/VarsAndData/Program.cs
@@ -7,7 +7,7 @@ namespace VarsAndData
         static void Main(string[] args)
         {
             // Declare some basic value type variables
-            int i = 10;
+            var i = 10;
             float f = 2.0f;
             decimal d = 10.0m;
             bool b = true;
@@ -21,13 +21,14 @@ namespace VarsAndData
             var z = "Hello!";
 
             // TODO: Declare an array of values
-
+                int[] vals = new int[5];
+                string[] strs = {"one", "two", "three"};
 
             // TODO: Print the values using a Formatting String
-
+                // Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7}", i,c,b,str,f,d,x,z);
 
             // TODO: "null" means "no value"
-
+            Console.WriteLine(z);
 
             // TODO: Implicit conversion between types
 
